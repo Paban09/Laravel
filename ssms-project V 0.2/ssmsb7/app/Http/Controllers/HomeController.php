3f0798141010471ff7270b62868917b1a8b61a70
Paban09 <masudranapaban22@gmail.com>
2022-09-09 13:08:07 +0600

    laravel

---
+++ ssms-project V 0.2/ssmsb7/app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+      public function index(){
+          return view('website.home.index');
+      }
+
+    public function about(){
+        return view('website.about.index');
+    }
+
+    public function courses(){
+        return view('website.courses.index');
+    }
+
+    public function contact(){
+        return view('website.contact.index');
+    }
+
+    public function auth(){
+        return view('website.auth.index');
+    }
+}
---
+++ ssms-project V 0.2/ssmsb7/resources/views/website/courses/index.blade.php
@@ -0,0 +1,125 @@
+@extends('website.master')
+@section('title')
+    Course
+@endsection
+
+@section('body')
+    <section>
+        <div class="container-fluid py-5 bg-secondary">
+            <div class="row">
+                <div class="col">
+                    <h1 class="text-center text-white">All Courses</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container py-5">
+
+            <div class="row">
+
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img src="{{asset('/')}}website/img/service_img2.jpg" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <h3>Responsive Web Design</h3>
+                            <h5>MD polash Mia</h5>
+                            <p>Tk. 12000</p>
+                            <p>Starting Date: 12.09.2022</p>
+                            <hr>
+                            <a href="" class="btn btn-success">Read more</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img src="{{asset('/')}}website/img/service_img2.jpg" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <h3>Responsive Web Design</h3>
+                            <h5>MD polash Mia</h5>
+                            <p>Tk. 12000</p>
+                            <p>Starting Date: 12.09.2022</p>
+                            <hr>
+                            <a href="" class="btn btn-success">Read more</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img src="{{asset('/')}}website/img/service_img2.jpg" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <h3>Responsive Web Design</h3>
+                            <h5>MD polash Mia</h5>
+                            <p>Tk. 12000</p>
+                            <p>Starting Date: 12.09.2022</p>
+                            <hr>
+                            <a href="" class="btn btn-success">Read more</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img src="{{asset('/')}}website/img/service_img2.jpg" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <h3>Responsive Web Design</h3>
+                            <h5>MD polash Mia</h5>
+                            <p>Tk. 12000</p>
+                            <p>Starting Date: 12.09.2022</p>
+                            <hr>
+                            <a href="" class="btn btn-success">Read more</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img src="{{asset('/')}}website/img/service_img2.jpg" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <h3>Responsive Web Design</h3>
+                            <h5>MD polash Mia</h5>
+                            <p>Tk. 12000</p>
+                            <p>Starting Date: 12.09.2022</p>
+                            <hr>
+                            <a href="" class="btn btn-success">Read more</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img src="{{asset('/')}}website/img/service_img2.jpg" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <h3>Responsive Web Design</h3>
+                            <h5>MD polash Mia</h5>
+                            <p>Tk. 12000</p>
+                            <p>Starting Date: 12.09.2022</p>
+                            <hr>
+                            <a href="" class="btn btn-success">Read more</a>
+                        </div>
+                    </div>
+                </div>
+
+                <nav aria-label="...">
+                    <ul class="pagination">
+                        <li class="page-item disabled">
+                            <span class="page-link">Previous</span>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item active">
+      <span class="page-link">
+        2
+        <span class="sr-only">(current)</span>
+      </span>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">Next</a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </div>
+    </section>
+@endsection
