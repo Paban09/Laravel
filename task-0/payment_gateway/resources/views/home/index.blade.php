f78331b4e47615475a39bc245690438e1d75c4e0
Paban09 <masudranapaban22@gmail.com>
2022-09-27 22:01:55 +0600

    laravel

---
+++ task-0/payment_gateway/resources/views/home/index.blade.php
@@ -0,0 +1,10 @@
+{{--ssl comerce and amr pay --}}
+{{--brac bank er bkash er API--}}
+
+<h1>Payment Gateway</h1>
+
+
+{{--intigrate API--}}
+{{-- individual vabe intigrate kora jae. API er through te --}}
+
+
